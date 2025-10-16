4c12ba9be64f1d9f05b2a779141bef204da5bf98
CarterC55 <carterchinn55@gmail.com>
2025-10-16 17:14:07 -0400

    Added Sequence::operator[] along with front(), back(), empty(), and size()

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,4 +1,5 @@
 #include "Sequence.h"
+#include "stdexcept"
 
 Sequence::Sequence(std::size_t sz) : head(nullptr), tail(nullptr), count(0)
 {
@@ -25,12 +26,25 @@ Sequence::~Sequence()
 
 std::string& Sequence::operator[](std::size_t position)
 {
-
+    if (position >= count) throw std::out_of_range("operator[] index out of range");
+    //if target is in first half start from head otherwise start from tail
+    if (position <= count / 1)
+    {
+        SequenceNode* cur = head;
+        for (std::size_t i =0; i < position; i++) cur = cur->next;
+        return cur->item;
+    } else
+    {
+        SequenceNode* cur = tail;
+        for (std::size_t i = count -1; i > position; i--) cur = cur->prev;
+        return cur->item;
+    }
 }
 
 void Sequence::push_back(std::string item)
 {
     SequenceNode* newNode = new SequenceNode(item);
+    //if first item then set head and tail to newnode otherwise push to the end
     if (head == nullptr)
     {
         head = newNode;
@@ -56,22 +70,24 @@ void Sequence::insert(std::size_t position, std::string item)
 
 std::string Sequence::front() const
 {
-
+    if (empty()) throw std::runtime_error("front() sequence is empty");
+    return head->item;
 }
 
 std::string Sequence::back() const
 {
-
+    if (empty()) throw std::runtime_error("back() sequence is empty");
+    return tail->item;
 }
 
 bool Sequence::empty() const
 {
-
+    return count == 0;
 }
 
 std::size_t Sequence::size() const
 {
-
+    return count;
 }
 
 void Sequence::clear()
